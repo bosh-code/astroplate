85a7f1b371572fd2995f08dcc65f23fcc861369c
bosh-code <ryan@bosher.co.nz>
2025-03-26 01:01:54 +1300

    fix use of any

--- src/lib/utils/sortFunctions.ts
+++ src/lib/utils/sortFunctions.ts
@@ -1,9 +1,8 @@
 // sort by date
 export const sortByDate = (array: any[]) => {
-  const sortedArray = array.sort(
+  return array.sort(
     (a: any, b: any) =>
       new Date(b.data.date && b.data.date).valueOf() -
       new Date(a.data.date && a.data.date).valueOf(),
   );
-  return sortedArray;
 };
