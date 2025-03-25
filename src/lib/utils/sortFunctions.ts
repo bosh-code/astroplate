1f86d9153249f1f49b83c3a121835c29d1804e09
bosh-code <ryan@bosher.co.nz>
2025-03-26 09:00:12 +1300

    update sort function

--- src/lib/utils/sortFunctions.ts
+++ src/lib/utils/sortFunctions.ts
@@ -1,8 +1,6 @@
-// sort by date
-export const sortByDate = (array: any[]) => {
-  return array.sort(
-    (a: any, b: any) =>
-      new Date(b.data.date && b.data.date).valueOf() -
-      new Date(a.data.date && a.data.date).valueOf(),
+import type { CollectionEntry } from "astro:content";
+
+export const sortByDate = <C extends string>(array: CollectionEntry<C>[]) =>
+  array.sort(
+    (a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf(),
   );
-};
